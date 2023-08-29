a48f7ec648a42531d13ff55ee7a05e4ad019513e
Vyacheslav Morev <gromd96@gmail.com>
2023-08-30 01:39:15 +0300

    фиксить ошибки бабеля

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useNavigate, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Register from '../Register/Register';
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,19 +11,31 @@ function Portfolio() {
         <li className="portfolio__list-item">
           <Link to="https://github.com/morevtrue/how-to-learn" className="portfolio__link" target="_blank">
             <h3 className="portfolio__list-title">Статичный сайт</h3>
-            <img src={linkPortfolioImg} className="portfolio__list-image" />
+            <img
+              src={linkPortfolioImg}
+              className="portfolio__list-image"
+              alt="ссылка на проект"
+            />
           </Link>
         </li>
         <li className="portfolio__list-item">
           <Link to="https://github.com/morevtrue/russian-travel" className="portfolio__link" target="_blank">
             <h3 className="portfolio__list-title">Адаптивный сайт</h3>
-            <img src={linkPortfolioImg} className="portfolio__list-image" />
+            <img
+              src={linkPortfolioImg}
+              className="portfolio__list-image"
+              alt="ссылка на проект"
+            />
           </Link>
         </li>
         <li className="portfolio__list-item">
           <Link to="https://github.com/morevtrue/express-mesto-gha" className="portfolio__link" target="_blank">
             <h3 className="portfolio__list-title">Одностраничное приложение</h3>
-            <img src={linkPortfolioImg} className="portfolio__list-image" />
+            <img
+              src={linkPortfolioImg}
+              className="portfolio__list-image"
+              alt="ссылка на проект"
+            />
           </Link>
         </li>
       </ul>
